27b35898d3b2ce2437084e4c96c41c87b43fb889
NguyenHoHoanMy-2003 <123210039@sv1.dut.udn.vn>
2025-09-22 16:02:08 +0700

    Thêm Route cho App

--- English-center-student-management-FE/src/App.jsx
+++ English-center-student-management-FE/src/App.jsx
@@ -3,18 +3,31 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./components/layouts/MainLayout.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import CRM from "./pages/CRM/index.jsx";
+import LoginPage from "./pages/Login/LoginPage.jsx";
+import ForgotPasswordPage from "./pages/Login/ForgotPasswordPage.jsx";
+import RegisterPage from "./pages/Login/RegisterPage.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/crm" element={<CRM />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </MainLayout>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/*"
+          element={
+            <MainLayout>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/crm" element={<CRM />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </MainLayout>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
