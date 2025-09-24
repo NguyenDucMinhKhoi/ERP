c876d38079f3e5bb0826f4c856f578f0bfaee14a
Anh Van <anhvandinh0@gmail.com>
2025-09-24 08:37:09 +0700

    add page Mainpage

--- English-center-student-management-FE/src/App.jsx
+++ English-center-student-management-FE/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./components/layouts/MainLayout.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import Mainpage from "./pages/Mainpage/Mainpage.jsx";
 import CRM from "./pages/CRM/index.jsx";
 
 export default function App() {
@@ -9,10 +10,11 @@ export default function App() {
     <BrowserRouter>
       <MainLayout>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Mainpage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/crm" element={<CRM />} />
           <Route path="*" element={<Navigate to="/" replace />} />
+
         </Routes>
       </MainLayout>
     </BrowserRouter>
