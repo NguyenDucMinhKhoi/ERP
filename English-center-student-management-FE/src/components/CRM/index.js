cde4813ee16c89343d370cc1400e30943a6d8b7d
Anh Van <anhvandinh0@gmail.com>
2025-09-30 15:25:22 +0700

    feat: thêm export StudentModules component

--- English-center-student-management-FE/src/components/CRM/index.js
+++ English-center-student-management-FE/src/components/CRM/index.js
@@ -3,3 +3,4 @@ export { default as CareLogForm } from './CareLogForm';
 export { default as StudentList } from './StudentList';
 export { default as StudentProfile } from './StudentProfile';
 export { default as CRMReports } from './CRMReports';
+export { default as StudentModules } from './StudentModules.jsx';
